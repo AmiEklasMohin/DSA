d0dfe9a9e3108ce6e6160d41297e819e04c78af9
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-20 00:10:48 +0600

    selection sort (naive sorting algorithm)

---
+++ selection_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = n - i;
+    }
+    // selection_sort
+    auto start_time = clock();
+    for (int i = 0; i < n; ++i) {
+        int index = i;
+        for (int j = (i + 1); j < n; ++j) {
+            if (arr[i] > arr[j]) {
+                index = j;
+            }
+        }
+        if (index != i) {
+            swap(arr[index], arr[i]);
+        }
+    }
+    auto finish_time = clock();
+    cout << "Runtime = "<< finish_time - start_time << "ms" << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+    return 0;
+}
+

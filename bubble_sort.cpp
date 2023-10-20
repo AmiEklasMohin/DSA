26dbc2df8356e4c8411122f5bb3edb60fd88a54e
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-20 16:24:24 +0600

    bubble sort (naive sorting algorithm)

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
-#include <algorithm>
 #include <ctime>
 using namespace std;
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int arr[], int n) {
+    auto start_time = clock();
+    for (int i = 0; i < (n - 1); ++i) {
+        for (int j = 0; j < (n - i - 1); ++j) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+    auto finish_time = clock();
+    cout <<"Runtime = "<< finish_time - start_time << "ms" << endl;
+}
 
 int main() {
     int n;
@@ -10,20 +29,7 @@ int main() {
     for (int i = 0; i < n; ++i) {
         arr[i] = n - i;
     }
-    // bubble_sort
-    auto start_time = clock();
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n - i - 1; ++j) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
-            }
-        }
-    }
-    auto finish_time = clock();
-    cout <<"Runtime = "<< finish_time - start_time << "ms" << endl;
-    for (int i = 0; i < n; ++i) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    bubbleSort(arr, n);
+    printArray(arr, n);
     return 0;
 }

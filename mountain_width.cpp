97b62014c2153fde4bb7f8cd059934dd45bc8324
EklasMohin <mohin@Eklas.mylinux.com>
2023-10-22 12:44:41 +0600

    Maximum Mountain Width

--- mountain_width.cpp
+++ mountain_width.cpp
@@ -3,36 +3,40 @@
 #include <vector>
 using namespace std;
 
-int maximumMountainWidth(vector<int> vectr) {
-    int maximum_width = 0;
-    int n = vectr.size();
-    for (int i = 1; i < (n - 1); ++i) {
-        if (vectr[i] > vectr[i + 1] and vectr[i] > vectr[i - 1]) {
-            int counter = 3, left = i - 1, right = i + 1;
-            while (vectr[left] > vectr[left - 1] and left > 0) {
-                counter += 1;
-                left -= 1;
+class Solution {
+public:
+    int maximumMountainWidth(vector<int>& mountain) {
+        int maximum_width = 0;
+        int n = mountain.size();
+        for (int i = 1; i < (n - 1); ++i) {
+            if (mountain[i] > mountain[i + 1] and mountain[i] > mountain[i - 1]) {
+                int counter = 3, left = i - 1, right = i + 1;
+                while (mountain[left] > mountain[left - 1] and left > 0) {
+                    counter += 1;
+                    left -= 1;
+                }
+                while (mountain[right] > mountain[right + 1] and right < (n - 1)) {
+                    counter += 1;
+                    right += 1;
+                }
+                i = right - 1;
+                maximum_width = max(maximum_width, counter);
             }
-            while (vectr[right] > vectr[right + 1] and right < (n - 1)) {
-                counter += 1;
-                right += 1;
-            }
-            i = right - 1;
-            maximum_width = max(maximum_width, counter);
         }
+        return maximum_width;
     }
-    return maximum_width;
-}
+};
 
 int main() {
-    int n, in;
-    cin >> n;
-    vector<int> vectr;
-    for (int i = 1; i <= n; ++i) {
+    int num_of_points, in;
+    cin >> num_of_points;
+    vector<int> mountain;
+    for (int i = 1; i <= num_of_points; ++i) {
         cin >> in;
-        vectr.push_back(in);
+        mountain.push_back(in);
     }
-    int maximum_width = maximumMountainWidth(vectr);
+    Solution ans;
+    int maximum_width = ans.maximumMountainWidth(mountain);
     cout<< maximum_width << endl;
     return 0;
 }
